980a97a666fc195a00b1873e2e476cea019f1d06
Michael Eldredge <michaelgeldredge@gmail.com>
2022-08-10 12:43:38 -0600

    started user profile componant

--- store/src/app/user.service.ts
+++ store/src/app/user.service.ts
@@ -4,6 +4,22 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class UserService {
+  userID: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  userName: string;
+  userPassword: string;
+  userRole: string;
 
-  constructor() { }
+  constructor() {
+    this.userID = 0;
+    this.firstName = "Null";
+    this.lastName = "User";
+    this.email = "nobody@email.com";
+    this.userName = "NullUser";
+    this.userPassword = "";
+    this.userRole = "testing";
+
+   }
 }
