b36a309d0ac2a16943e2578303c28d219cd527ad
Rheem08 <raheen902@revature.net>
2022-08-15 09:03:23 -0400

    attempted put method for user, getting Cors error

--- store/src/app/login/login.component.ts
+++ store/src/app/login/login.component.ts
@@ -21,7 +21,7 @@ export class LoginComponent implements OnInit {
 
   constructor(private bookyService: BookyService, private fb: FormBuilder, 
     private http: HttpClient, private router: Router, public myUser:UserService){
-
+      
     }
   booky: Booky | any;
 
@@ -32,26 +32,30 @@ initializeForm(): void{
   this.loginForm = this.fb.group({
     username: [''],
     p_word: ['']
+
   })
   
 }
 onSubmit(){
   this.http.get<any>("http://localhost:2022/booky").subscribe(res=>{ //connects the RestAPI, performs get method
     const user = res.find((a:any)=>{ //inside of response we are assigned the values found at the resource in the response to the user
-      const usernames = a.username;
-      const p_words = a.p_word;
-      const emails = a.email;
-      const f_names = a.f_name;
-      const l_names = a.l_name;
+      const username = a.username;
+      const p_word = a.p_word;
+      const email = a.email;
+      const f_name = a.f_name;
+      const l_name = a.l_name;
+      let xUse = a.id; //trying to grab user id
       let temp:Booky;
       var returnme:boolean = false;
       if (a.username === this.loginForm.value.username){
         if(a.p_word === this.loginForm.value.p_word){
           returnme = true;
+          this.myUser.userID = a.id // assinging id          
           this.myUser.email = a.email;
           this.myUser.firstName = a.f_name;
           this.myUser.lastName = a.l_name;
           this.myUser.userName = this.loginForm.value.username;
+          
         }        
       }
       // return this.myUser.userName === this.loginForm.value.username && this.myUser.userPassword === this.loginForm.value.p_word;
